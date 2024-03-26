b60151f176d88699b6b343d7eabe79fc4835bb5e
Duron27 <duron27@gmail.com>
2024-03-26 18:23:41 -0400

    Update FragmentSettings.kt

--- app/src/main/java/ui/fragments/FragmentSettings.kt
+++ app/src/main/java/ui/fragments/FragmentSettings.kt
@@ -23,6 +23,7 @@ package ui.fragments
 import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
@@ -92,85 +93,39 @@ class FragmentSettings : PreferenceFragment(), OnSharedPreferenceChangeListener 
             }
         }
 
-        0
-
-
-I changed the first part to
-
-findPreference("game_files").setOnPreferenceClickListener {
+        findPreference("game_files").setOnPreferenceClickListener {
             // Use ACTION_OPEN_DOCUMENT_TREE intent to allow the user to choose a directory
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
             startActivityForResult(intent, REQUEST_CODE_OPEN_DOCUMENT_TREE)
             true
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if (requestCode == REQUEST_CODE_OPEN_DOCUMENT_TREE && resultCode == Activity.RESULT_OK) {
-        data?.data?.also { uri ->
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_OPEN_DOCUMENT_TREE && resultCode == Activity.RESULT_OK) {
+            data?.data?.also { uri ->
 
-            val selectedDirectory = DocumentFile.fromTreeUri(activity, uri) ?: return
-            val iniFile = selectedDirectory.findFile("Morrowind.ini")
-            val dataFilesFolder = selectedDirectory.findFile("Data Files")
-            val sharedPref = preferenceScreen.sharedPreferences
+                val selectedDirectory = DocumentFile.fromTreeUri(activity, uri) ?: return
+                val path = uri.path
+                val iniFile = selectedDirectory.findFile("Morrowind.ini")
+                val dataFilesFolder = selectedDirectory.findFile("Data Files")
+                val sharedPref = preferenceScreen.sharedPreferences
+                var gameFiles = path
 
-
-            if (iniFile != null && dataFilesFolder != null && dataFilesFolder.isDirectory) {
-                val gameFilesPreference = findPreference("game_files")
-                val path = uri.toString() // Convert Uri to String path
-                gameFilesPreference?.summary = path
-                with(sharedPref.edit()) {
-                    var gameFiles = path
-                    putString("game_files", gameFiles)
-                    apply()
-                }
-            } else {
-                showError(R.string.data_error_title, R.string.data_error_message)
-            }
-        }
-    }
-}
-
-
-    /**
-     * Checks the specified path for a valid morrowind installation, generates config files
-     * and saves the path to shared prefs if it's valid.
-     * If it isn't, an error is displayed to the user.
-     */
-    private fun setupData(path: String) {
-        val sharedPref = preferenceScreen.sharedPreferences
-
-        // reset the setting so that it's erased on error instead of keeping
-        // possibly stale value
-        var gameFiles = ""
-
-        val inst = GameInstaller(path)
-        if (inst.check()) {
-            val iniFile = File(path, "Morrowind.ini")
-            val dataFilesFolder = File(path, "Data Files")
-
-            if (iniFile.exists() && dataFilesFolder.exists() && dataFilesFolder.isDirectory) {
-                inst.setNomedia()
-                if (!inst.convertIni(sharedPref.getString("pref_encoding", GameInstaller.DEFAULT_CHARSET_PREF)!!)) {
-                    showError(R.string.data_error_title, R.string.ini_error_message)
+                if (iniFile != null && dataFilesFolder != null && dataFilesFolder.isDirectory) {
+                    val gameFilesPreference = findPreference("game_files")
+                    gameFilesPreference?.summary = path
+                    with(sharedPref.edit()) {
+                        putString("game_files", gameFiles)
+                        apply()
+                    }
                 } else {
-                    gameFiles = path
+                    showError(R.string.data_error_title, R.string.data_error_message)
                 }
-            } else {
-                // If either Morrowind.ini or Data Files folder is missing
-                showError(R.string.data_error_title, R.string.data_error_message)
             }
-        } else {
-            showError(R.string.data_error_title, R.string.data_error_message,
-                "https://omw.xyz.is/game.html")
-        }
-
-        with(sharedPref.edit()) {
-            putString("game_files", gameFiles)
-            apply()
         }
     }
-
 
     /**
      * Shows an alert dialog displaying a specific error
@@ -217,7 +172,7 @@ findPreference("game_files").setOnPreferenceClickListener {
             return
         if (preference is EditTextPreference) {
             if (key == "pref_uiScaling" && (preference.text == null || preference.text.isEmpty()))
-                // Show "Auto (1.23)"
+            // Show "Auto (1.23)"
                 preference.summary = MyApp.app.getString(R.string.uiScaling_auto)
                     .format(Locale.ROOT, MyApp.app.defaultScaling)
             else
@@ -235,18 +190,8 @@ findPreference("game_files").setOnPreferenceClickListener {
     private fun updateGammaState() {
         val sharedPref = preferenceScreen.sharedPreferences
         findPreference("pref_gamma").isEnabled =
-                sharedPref.getString("pref_graphicsLibrary_v2", "") != "gles1"
-/*
-	var isnohighpenabled = false;
-        if(sharedPref.getString("pref_shadersDir_v2", "") == "modified")
-		isnohighpenabled = true
-        findPreference("pref_nohighp").isEnabled = isnohighpenabled
+            sharedPref.getString("pref_graphicsLibrary_v2", "") != "gles1"
 
-	var isAdditionalAnimSourcesEnabled = false;
-        if(sharedPref.getBoolean("gs_use_additional_animation_sources", false) == true) isAdditionalAnimSourcesEnabled = true
-        findPreference("gs_weapon_sheating").isEnabled = isAdditionalAnimSourcesEnabled
-        findPreference("gs_shield_sheating").isEnabled = isAdditionalAnimSourcesEnabled
-*/
     }
-
 }
+
